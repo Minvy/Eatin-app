9c624a3e21cf736f933a8999223208804dcbc770
Minvy <52128108+Minvy@users.noreply.github.com>
2021-06-03 13:37:00 +0100

    Update firebase.js

--- app/src/utils/firebase.js
+++ app/src/utils/firebase.js
@@ -9,14 +9,14 @@ import "firebase/storage";
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBjz8Jc2xn3BVpR1jW0fZYJnXxPOUgazCc",
-  authDomain: "eatin-d731f.firebaseapp.com",
-  databaseURL: "https://eatin-d731f.firebaseio.com",
-  projectId: "eatin-d731f",
-  storageBucket: "eatin-d731f.appspot.com",
-  messagingSenderId: "240925319711",
-  appId: "1:240925319711:web:842884c0ef4e1e9ec6ab2a",
-  measurementId: "G-XFJYZHEKDB"
+  apiKey: "#",
+  authDomain: "#",
+  databaseURL: "#",
+  projectId: "#",
+  storageBucket: "#",
+  messagingSenderId: "#",
+  appId: "#",
+  measurementId: "#"
 };
 
 // Initialize Firebase
